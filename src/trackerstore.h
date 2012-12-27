05387c1332b218bd76fb1095b2a960ff24634bc7
Mohammed Sameer <msameer@foolab.org>
2012-12-28 00:28:51 +0200

    Added mime type to TrackerStore

--- src/trackerstore.h
+++ src/trackerstore.h
@@ -24,6 +24,7 @@
 #define TRACKER_STORE_H
 
 #include <QObject>
+#include <QHash>
 
 class QSparqlConnection;
 
@@ -57,6 +58,8 @@ signals:
 
 private:
   bool execQuery(const QString& query, const QString& path);
+
+  QHash<QString, QString> m_mime;
 
   QSparqlConnection *m_connection;
   QString m_manufacturer;
