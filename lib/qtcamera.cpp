84af8c99a6c04eafc7a19c2d0a6bc0a701cc662d
Mohammed Sameer <msameer@foolab.org>
2013-09-12 03:36:33 +0300

    Revert "Move qt_cam_copy_register to QtCamera"
    This reverts commit 9ad5a2d40dabd90687e8851fe336a32155be5933.

--- lib/qtcamera.cpp
+++ lib/qtcamera.cpp
@@ -22,15 +22,10 @@
 #include "qtcamscanner.h"
 #include "qtcamconfig.h"
 #include "qtcamdevice.h"
-#include "gst/gstcopy.h"
 #include <gst/gst.h>
 
 class QtCameraPrivate {
 public:
-  QtCameraPrivate() {
-    qt_cam_copy_register();
-  }
-
   QtCamConfig *conf;
   QtCamScanner *scanner;
 };
