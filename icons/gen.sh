ad07b8f7ab183460c83b9b8020af9d7229169a5e
Mohammed Sameer <msameer@foolab.org>
2013-12-13 23:36:53 +0200

    Updated icon. We also use .ai file instead of the svg for the logo

--- icons/gen.sh
+++ icons/gen.sh
@@ -1,4 +1,4 @@
- cat icons.txt| while read l; do echo $l | sed -e 's/^\([^ ]*\) \([0-9]*\) \([0-9]*\) \(.*\)/inkscape -C -e cameraplus-\1.png -w \2 -h \3 \4/'; done | bash
+cat icons.txt| while read l; do echo $l | sed -e 's/^\([^ ]*\) \([0-9]*\) \([0-9]*\) \(.*\)/inkscape -C -e cameraplus-\1.png -w \2 -h \3 \4/'; done | bash
 
 cat <<EOF  > icons.qrc
 <!DOCTYPE RCC>
