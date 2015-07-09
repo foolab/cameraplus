c9796b7fad2ae52f2b955511cea4838803772cb7
Mohammed Sameer <msameer@foolab.org>
2015-07-10 00:13:02 +0300

    mosaic_renderer.h: Add Android copyright blurb and add include guards

--- panorama/mosaic_renderer.h
+++ panorama/mosaic_renderer.h
@@ -1,4 +1,22 @@
-#pragma once
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MOSAIC_RENDERER_H
+#define MOSAIC_RENDERER_H
+
 #include <semaphore.h>
 
 // The Preview FBO dimensions are determined from the high-res
@@ -33,3 +51,5 @@ extern int gPreviewImageWidth[NR];
 extern int gPreviewImageHeight[NR];
 
 extern sem_t gPreviewImage_semaphore;
+
+#endif /* MOSAIC_RENDERER_H */
