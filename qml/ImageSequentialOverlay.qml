foolab/cameraplus

commit 91429c475a7152b2c3a66f9810b7763493983d02

UI scalability fixes
Move a lot of hardcoded sizes to harmattan CameraStyle and tweak the
SailfishOS CameraStyle to use constants from Silica Theme.

qml/ImageSequentialOverlay.qml
@@ -69,7 +69,7 @@ BaseOverlay {
             color: "white"
             styleColor: "black"
             style: Text.Outline
-            font.pixelSize: 36
+            font.pixelSize: cameraStyle.fontSizeLarge
             text: qsTr("Remaining shots: %1").arg(remainingShots)
             visible: remainingShots > 0
         }
@@ -83,7 +83,7 @@ BaseOverlay {
             color: "white"
             styleColor: "black"
             style: Text.Outline
-            font.pixelSize: 36
+            font.pixelSize: cameraStyle.fontSizeLarge
 
             Timer {
                 id: countDownTimer
@@ -121,13 +121,13 @@ BaseOverlay {
 
     Column {
         visible: controlsVisible && !overlayCapturing && !selectedLabel.visible
-        spacing: 20
+        spacing: cameraStyle.spacingLarge
         anchors {
             verticalCenter: parent.verticalCenter
             left: settings.leftHandedMode ? undefined : parent.left
-            leftMargin: 20
+            leftMargin: cameraStyle.padding
             right : settings.leftHandedMode ? parent.right : undefined
-            rightMargin: 20
+            rightMargin: cameraStyle.padding
         }
 
         OnScreenOption {
@@ -173,8 +173,10 @@ BaseOverlay {
 
     CameraToolBarLabel {
         id: selectedLabel
-        anchors.bottom: toolBar.top
-        anchors.bottomMargin: 20
+        anchors {
+            bottom: toolBar.top
+            bottomMargin: cameraStyle.padding
+        }
         visible: controlsVisible && !overlayCapturing && text != ""
     }
 

qml/ImageTimerOverlay.qml
@@ -56,7 +56,7 @@ BaseOverlay {
         color: "white"
         styleColor: "black"
         style: Text.Outline
-        font.pixelSize: 36
+        font.pixelSize: cameraStyle.fontSizeLarge
 
         onVisibleChanged: {
             if (visible) {
@@ -109,9 +109,9 @@ BaseOverlay {
         anchors {
             verticalCenter: parent.verticalCenter
             left: settings.leftHandedMode ? undefined : parent.left
-            leftMargin: 20
+            leftMargin: cameraStyle.padding
             right : settings.leftHandedMode ? parent.right : undefined
-            rightMargin: 20
+            rightMargin: cameraStyle.padding
         }
 
         text: qsTr("%1s").arg(settings.captureTimerDelay)
@@ -135,8 +135,10 @@ BaseOverlay {
 
     CameraToolBarLabel {
         id: selectedLabel
-        anchors.bottom: toolBar.top
-        anchors.bottomMargin: 20
+        anchors {
+            bottom: toolBar.top
+            bottomMargin: cameraStyle.padding
+        }
         visible: controlsVisible && !overlayCapturing && text != ""
     }
 

qml/OnScreenOption.qml
@@ -33,17 +33,17 @@ Item {
     property alias valueIndicatorText: slider.valueIndicatorText
     property bool __showSlider: false
 
-    width: 80
-    height: 80
+    width: cameraStyle.onScreenOptionWidth
+    height: cameraStyle.onScreenOptionHeight
 
     Rectangle {
         id: rect
         anchors.fill: parent
-        color: mouse.pressed ? cameraStyle.pressedColor : "black"
+        color: mouse.pressed ? cameraStyle.pressedColor : cameraStyle.backgroundColor
         opacity: 0.5
         radius: 40
         border.width: 1
-        border.color: "gray"
+        border.color: cameraStyle.borderColor
 
         MouseArea {
             id: mouse
@@ -54,7 +54,7 @@ Item {
         CameraLabel {
             id: label
             anchors.centerIn: parent
-            font.pixelSize: 36
+            font.pixelSize: cameraStyle.fontSizeLarge
             font.bold: true
         }
     }
@@ -64,9 +64,9 @@ Item {
         anchors {
             verticalCenter: rect.verticalCenter
             left: settings.leftHandedMode ? undefined : rect.right
-            leftMargin: 20
+            leftMargin: cameraStyle.padding
             right : settings.leftHandedMode ? rect.left : undefined
-            rightMargin: 20
+            rightMargin: cameraStyle.padding
         }
         visible: opacity > 0.0
         opacity: rootWindow.active && __showSlider ? 1.0 : 0.0

qml/BaseOverlay.qml
@@ -98,7 +98,7 @@ Item {
 
         anchors {
             top: capture.bottom
-            topMargin: 20
+            topMargin: cameraStyle.padding
         }
 
         visible: controlsVisible && !overlayCapturing
@@ -128,8 +128,8 @@ Item {
             rightMargin: capture.x + ((capture.width - modeButton.width) / 2)
             top: parent.top
             bottom: parent.bottom
-            topMargin: 20
-            bottomMargin: 20
+            topMargin: cameraStyle.padding
+            bottomMargin: cameraStyle.padding
         }
     }
 

qml/CameraInfoBanner.qml
@@ -30,8 +30,8 @@ Rectangle {
     height: label.height * 2
     y: shown ? 8 : - (height + 8)
     anchors.horizontalCenter: parent.horizontalCenter
-    color: "black"
-    border.color: "gray"
+    color: cameraStyle.backgroundColor
+    border.color: cameraStyle.borderColor
     radius: 20
     opacity: shown ? 1.0 : 0
 

qml/CameraSlider.qml
@@ -35,8 +35,8 @@ MouseArea {
     property string valueIndicatorText: value.toFixed(1)
     property bool handleVisible: true
 
-    width: 500
-    height: 40
+    width: parent.width / 2
+    height: cameraStyle.sliderHeight
 
     RangeModel {
         id: range

qml/CameraToolBar.qml
@@ -98,8 +98,8 @@ Rectangle {
 
     width: expanded ? targetWidth : menu.width
     height: menu.height
-    color: "black"
-    border.color: "gray"
+    color: cameraStyle.backgroundColor
+    border.color: cameraStyle.borderColor
     radius: 20
 
     Behavior on width {

qml/CameraToolIcon.qml
@@ -24,8 +24,8 @@ import QtQuick 2.0
 
 Rectangle {
     color: "transparent"
-    width: 64
-    height: 64
+    width: cameraStyle.toolIconWidth
+    height: cameraStyle.toolIconHeight
     opacity: enabled ? 1.0 : 0.5
 
     property alias pressed: mouse.pressed

commit 32c32a6a464df4487a9c27ce2c71a6424d64acf6

Make sure we set a proper width for OnScreenOption slider
This is a regression introduced by commit 91429c475a7152b2c3a66f9810b7763493983d02

qml/ImageSequentialOverlay.qml
@@ -132,6 +132,7 @@ BaseOverlay {
 
         OnScreenOption {
             id: count
+            visualParent: overlay
             text: qsTr("%1x").arg(settings.sequentialShotsCount)
             minimumValue: 2
             maximumValue: 20
@@ -152,6 +153,7 @@ BaseOverlay {
 
         OnScreenOption {
             id: interval
+            visualParent: overlay
             text: qsTr("%1s").arg(settings.sequentialShotsInterval)
             minimumValue: 0
             maximumValue: 60

qml/ImageTimerOverlay.qml
@@ -105,6 +105,7 @@ BaseOverlay {
 
     OnScreenOption {
         id: delay
+        visualParent: overlay
 
         anchors {
             verticalCenter: parent.verticalCenter

qml/OnScreenOption.qml
@@ -32,6 +32,7 @@ Item {
     property alias valueIndicatorVisible: slider.valueIndicatorVisible
     property alias valueIndicatorText: slider.valueIndicatorText
     property bool __showSlider: false
+    property Item visualParent
 
     width: cameraStyle.onScreenOptionWidth
     height: cameraStyle.onScreenOptionHeight
@@ -61,6 +62,7 @@ Item {
 
     CameraSlider {
         id: slider
+        width: visualParent.width / 2
         anchors {
             verticalCenter: rect.verticalCenter
             left: settings.leftHandedMode ? undefined : rect.right