27bc2569fbeadb265925e6e029a0a465bf4d6f59
Mohammed Sameer <msameer@foolab.org>
2016-08-20 03:48:11 +0300

    Add a target to extract plural forms and add the ts file with plurals

---
+++ translations/plurals.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en">
+<context>
+    <name>ImageSequentialOverlay</name>
+    <message numerus="yes">
+        <location filename="../qml/ImageSequentialOverlay.qml" line="150"/>
+        <source>Capture %n shots</source>
+        <comment>0</comment>
+        <translation>
+            <numerusform>Capture %n shot</numerusform>
+            <numerusform>Capture %n shots</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/ImageSequentialOverlay.qml" line="171"/>
+        <source>%n seconds interval</source>
+        <comment>0</comment>
+        <translation>
+            <numerusform>%n second interval</numerusform>
+            <numerusform>%n seconds interval</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>ImageSequentialShotsSettings</name>
+    <message numerus="yes">
+        <location filename="../qml/ImageSequentialShotsSettings.qml" line="61"/>
+        <source>%n seconds</source>
+        <comment>0</comment>
+        <translation>
+            <numerusform>%n second</numerusform>
+            <numerusform>%n seconds</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>ImageTimerOverlay</name>
+    <message numerus="yes">
+        <location filename="../qml/ImageTimerOverlay.qml" line="133"/>
+        <source>Capture after %n seconds</source>
+        <comment>0</comment>
+        <translation>
+            <numerusform>Capture after %n second</numerusform>
+            <numerusform>Capture after %n seconds</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
